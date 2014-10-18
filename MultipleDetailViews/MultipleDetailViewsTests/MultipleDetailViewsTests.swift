bda37b8d4d69ff2f1dc20d815cf0668e9e1dfae1
Sachin Palewar <sachin@palewar.com>
2014-10-18 16:06:43 +0530

    Master Detail Application Project template
    Code and files which you get automatically when you create  a new
    ‘Master Detail Application’ project in Xcode 6. No code is changed and
    no additional files added right now.

---
+++ MultipleDetailViews/MultipleDetailViewsTests/MultipleDetailViewsTests.swift
@@ -0,0 +1,36 @@
+//
+//  MultipleDetailViewsTests.swift
+//  MultipleDetailViewsTests
+//
+//  Created by Sachin Palewar on 10/18/14.
+//  Copyright (c) 2014 PTS. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class MultipleDetailViewsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
